5c6ce7a6bca2a534a1b03d96b234d8cc4a03ebcf
Luis Chang <luis.chang@outlook.com>
2023-12-20 20:43:01 -0500

    Agregar archivos de proyecto.

---
+++ Cibertec.Shopping.WEB/Controllers/SecurityController.cs
@@ -0,0 +1,49 @@
+﻿using Cibertec.Shopping.WEB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Json;
+
+namespace Cibertec.Shopping.WEB.Controllers
+{
+    public class SecurityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel) 
+        {
+            if (ModelState.IsValid)
+            {
+                var registerEndpoint = "http://localhost:5261/api/User";
+
+                using var httpClient = new HttpClient();
+                var jsonContent = new StringContent(JsonSerializer.Serialize(registerViewModel)
+                    , Encoding.UTF8, "application/json");
+
+                var response = await httpClient.PostAsync(registerEndpoint, jsonContent);
+
+                if (response.IsSuccessStatusCode)
+                    return RedirectToAction("Login", "Security");
+                else
+                    ModelState.AddModelError(string.Empty, "Error al registrar el usuario, inténtelo nuevamente");
+
+
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Cibertec.Shopping.WEB/Models/RegisterViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Cibertec.Shopping.WEB.Models
+{
+    public class RegisterViewModel
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public DateTime dateOfBirth { get; set; }
+        public string country { get; set; }
+        public string address { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
